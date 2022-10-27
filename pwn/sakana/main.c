c5a46caf2f040e7db87c934221b3d1b46e3071c3
elite3312 <perrywu12@gmail.com>
2022-10-27 18:33:58 +0800

    update

--- pwn/sakana/main.c
+++
@@ -1,5 +0,0 @@
-int __cdecl __noreturn main(int argc, const char **argv, const char **envp)
-{
-  banner(argc, argv, envp);
-  shell();
-}
--- pwn/sakana/shell.c
+++
@@ -1,9 +0,0 @@
-void __noreturn shell()
-{
-  while ( 1 )
-  {
-    get_line("~> ", &cmd);
-    parse_cmd();
-    memset(&cmd, 0LL, 512LL);
-  }
-}
