9362c2fd0dee6fe0bdde21f40795c92d973a1aec
elite3312 <perrywu12@gmail.com>
2022-11-09 11:39:13 +0800

    complete project_alicization hit local

--- pwn/project_alicization/test_shellcode.py
+++
@@ -1,8 +0,0 @@
-from pwn import *
-
-
-#print(pwnlib.shellcraft.amd64.linux.syscall('SYS_execve', 1, 'rsp', 2, 0).rstrip())
-
-with context.local():
-    context.arch = 'amd64'
-    print(enhex(asm(shellcraft.pushstr("/bin/sh"))))
